45a0e3920e1758204c52e41a93730fce49435b72
joao.araujo@tellks.com.br <joao.araujo@tellks.com.br@bc2a25e6-0000-d4ec-7a5c-07b964c5390b>
2014-07-21 19:40:25 +0000

    Condicional

--- branches/application/controllers/welcome.php
+++ branches/application/controllers/welcome.php
@@ -17,11 +17,12 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+	public function index() {
+        if (1 == 1) {
             echo 'hello world';
-		$this->load->view('welcome_message');
-	}
+        }
+        $this->load->view('welcome_message');
+    }
 }
 
 /* End of file welcome.php */

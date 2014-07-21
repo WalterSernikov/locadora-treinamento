81cb2f20b4aa7678acfc017b1c25ee6aed35e392
waltersernikov@yahoo.com.br <waltersernikov@yahoo.com.br@bc2a25e6-0000-d4ec-7a5c-07b964c5390b>
2014-07-21 19:38:52 +0000

    ingreis

--- branches/application/controllers/welcome.php
+++ branches/application/controllers/welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-            echo 'Ola mundo';
+            echo 'hello world';
 		$this->load->view('welcome_message');
 	}
 }

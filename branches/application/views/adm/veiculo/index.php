f20192c6c0713f0e57aa97af3f6e72cc9295f713
waltersernikov@yahoo.com.br <waltersernikov@yahoo.com.br@bc2a25e6-0000-d4ec-7a5c-07b964c5390b>
2014-07-23 17:10:47 +0000

    CRUD Marca

---
+++ branches/application/views/adm/veiculo/index.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+echo _mensagem_flashdata();
+?>
+
+<div class="row col-lg-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <a class="btn btn-primary" href="<?php echo base_url($this->config->item('area_admin') . '/veiculo/cadastrar'); ?>">Cadastrar</a>
+        </div> 
+        <div class="panel-body">
+
+            <?php
+            $this->table->set_heading('Id', 'Modelo', 'Cor', 'Placa', 'Ações');
+
+            foreach ($veiculo as $v) {
+
+                $link_editar  = base_url($this->config->item('area_admin') . '/veiculo/editar/' . $v->id);
+                
+                $acoes  = '<a href="' . $link_editar . '" class="btn btn-info btn-sm">Editar</a>&nbsp;';
+                $acoes .= '<a href="#" data-id="' . $v->id . '" data-toggle="modal" data-target="#modal_confirmar_remocao" class="btn btn-danger btn-sm btn_remover">Remover</a>';
+                
+                $this->table->add_row(
+                        $v->id, $v->modelo, $v->cor, $v->placa, $acoes
+                );
+            }
+
+            $this->table->set_template(array(
+                'table_open' => '<table class="table table-striped table-bordered table-hover">',
+            ));
+
+            echo $this->table->generate();
+            ?>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modal_confirmar_remocao" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Remover veiculo</h4>
+            </div>
+            <div class="modal-body">
+                <p>Você realmente deseja remover este veiculo?</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="btn btn-default" data-dismiss="modal">Não</a>
+                <a href="<?php echo base_url($this->config->item('area_admin') . '/veiculo/remover/'); ?>" id="confirma_remocao" class="btn btn-primary">Sim</a>
+            </div>
+        </div>
+    </div>
+</div>
+

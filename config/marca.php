c3a68402ff72d1903efd08175685704a2bf0cc86
waltersernikov <waltersernikov@yahoo.com.br>
2014-07-23 14:15:39 +0000

    CRUD Marca

--- config/marca.php
+++ config/marca.php
@@ -1,10 +1,9 @@
-<?php
+<?php if (!defined('BASEPATH'))  exit('No direct script access allowed');
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$config['regras_validacao'] = array(
+    array(
+        'field' => 'nome',
+        'label' => 'nome',
+        'rules' => 'trim|required'
+    ),
+);
--- views/adm/marca/editar.php
+++ views/adm/marca/editar.php
@@ -1,10 +1,32 @@
-<?php
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');?>
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="col-lg-12">
+    <?php
+    echo form_open(base_url($this->config->item('area_admin') . '/marca/salvar'), 'id="editar_marca"'); 
+    
+    echo form_hidden('id', (isset($marca->id))? $marca->id: set_value('id'));
+    ?>
+    
+    <div class="row">
+        <div class="form-group col-lg-6">
+            <?php
+            echo form_label('Nome *');
+            echo form_input('nome', (isset($marca->nome)? $marca->nome: set_value('nome')), 'id="nome" class="form-control"');
+            echo form_error('nome');
+            ?>
+        </div> 
+        
+    </div>
+    <div class="row" style="padding-top: 20px;">
+        <div class="form-group col-lg-6">
+            <?php 
 
+            echo form_submit('salvar','Salvar', 'class="btn btn-primary"');
+
+            echo '&nbsp;' . form_button('cancelar','Cancelar', 'class="btn btn-danger btn_cancelar"');
+            ?>
+        </div>
+    </div>
+    
+    <?php echo form_close(); ?>
+</div> 

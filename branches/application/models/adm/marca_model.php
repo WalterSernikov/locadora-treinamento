48762734f6cbc9558caa957153e2d46570a60cda
waltersernikov@yahoo.com.br <waltersernikov@yahoo.com.br@bc2a25e6-0000-d4ec-7a5c-07b964c5390b>
2014-07-23 14:15:39 +0000

    CRUD Marca

--- branches/application/models/adm/marca_model.php
+++ branches/application/models/adm/marca_model.php
@@ -13,6 +13,100 @@ if (!defined('BASEPATH'))
  *
  * @author tellks
  */
-class marca_model {
-    //put your code here
+class Marca_model extends CI_Model{
+    private $tabela;
+    
+    function __construct() {
+        parent::__construct();
+        
+        $this->tabela = 'marca';
+    }
+            
+    /**
+     * Busca todos os marca do banco de dados
+     * 
+     * @return array
+     */
+    function get_all(){
+        
+        $marca = $this->db->get($this->tabela);
+        
+        if($marca->num_rows() > 0){
+            
+            return $marca->result();
+        }
+        else {
+            
+            return array();
+        }
+    }
+    
+    /**
+     * Busca os dados de um marca pelo seu ID
+     * 
+     * @param int $id ID do marca
+     * @return mixed
+     */
+    function get_by_id($id){
+        
+        $this->db->select('*');
+        
+        $this->db->from($this->tabela);
+        
+        $this->db->where('id', (int)$id);
+        
+        $marca = $this->db->get();
+        
+        if($marca->num_rows() > 0){
+            
+            return $marca->row(0);
+        }
+        else{
+            
+            return false;
+        }
+    }
+            
+    /**
+     * Insere um marca no banco de dados
+     * 
+     * @param sdtClass Object $marca Objeto contendo os dados do marca
+     * @return boolean
+     */
+    function inserir($marca){
+        
+        $this->db->insert($this->tabela,$marca);
+        
+        return (bool)$this->db->affected_rows();
+    }
+    
+    /**
+     * atualiza um marca no banco de dados
+     * 
+     * @param sdtClass Object $marca Objeto contendo os dados do marca
+     * @return boolean
+     */
+    function atualizar($marca){
+        
+        $this->db->where('id', (int)$marca->id);
+        
+        $this->db->update($this->tabela,$marca);
+        
+        return (bool)$this->db->affected_rows();
+    }
+    
+    /**
+     * Remove um marca do banco de dados
+     * 
+     * @param int $id ID do marca a ser removido
+     * @return boolean
+     */
+    function remover($id){
+        
+        $this->db->where('id', (int)$id);
+        
+        $this->db->delete($this->tabela);
+        
+        return (bool)$this->db->affected_rows();
+    }
 }
